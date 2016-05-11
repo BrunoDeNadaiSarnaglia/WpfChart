70ba06d00a00e06e30f294bf0dc24b0fd012b1fc
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-05-11 01:13:11 -0300

    HTTP Get random numbers

--- ChartApplicationWPF/MainWindow.xaml.cs
+++ ChartApplicationWPF/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -24,7 +26,6 @@ namespace ChartApplicationWPF
     public partial class MainWindow : Window
     {
         double x = 0;
-        double y = 0;
         Points points = new Points();
         public MainWindow()
         {
@@ -35,17 +36,32 @@ namespace ChartApplicationWPF
             line.IndependentValueBinding = new Binding("Key");
             line.DataContext = points;
             Chart.Series.Add(line);
-            Timer timer = new Timer(1000);
+            Timer timer = new Timer(5000);
             timer.Enabled = true;
             timer.Elapsed += this.OnTimer; 
         }
 
         private void OnTimer(object asdsa, EventArgs ard){
-            x += 10;
-            y += 10;
+            string url = "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.UserAgent = "lamdaskmd";
+            
+            double y = default(double);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream resStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(resStream))
+                    {
+                        y = (double)int.Parse(reader.ReadLine());
+                    }
+                }
+            }
+
             this.Dispatcher.Invoke((Action)(() =>
             {
-                points.AddPoint(x, y);
+                points.AddPoint(x++, y);
                 if(x == 200)    points.MaxNumberPoints = 10;
             }));        
         }
